98ae5da404a74082762be7ab78c3c6b969b2095c
khadija <khadijalahmar8@gmail.com>
2022-03-28 21:24:32 +0000

    ✅ Add file Router and modification in file server .js

--- server.js
+++ server.js
@@ -1,8 +1,16 @@
 const express =require('express');
 const app = express();
 
-// connexion database 
+// import userRoute
+const userRoute = require('./routes/user');
+// midlware 
+app.use('/', userRoute);
+
+// database connection
 const database = require('./config/database');
+
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
 
 database.authenticate()
 .then(()=>console.log('Database connect'))
