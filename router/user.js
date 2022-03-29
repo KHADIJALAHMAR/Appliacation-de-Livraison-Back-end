95bdf92b49eb66c4a9e1f5afbde4dea85d12b78b
khadija <khadijalahmar8@gmail.com>
2022-03-29 17:23:35 +0000

    ✅ Create Route Login and Register

--- router/user.js
+++ router/user.js
@@ -1,6 +1,15 @@
 const express = require('express');
 const router = express.Router();
+const UserController = require('../controllers/UserController');
 
+// Créate Route Login
 
-// user Routers
+router.route('/login')
+.get(UserController.Login)
+.post(UserController.validation_login);
 
+// Créate Route Register
+
+router.route('/register')
+.get(UserController.Register )
+.post(UserController.validation_register);
