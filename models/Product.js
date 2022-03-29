9baec2e73fdbe01281260f67d721cccc100e988f
khadija <khadijalahmar8@gmail.com>
2022-03-29 10:14:53 +0000

    ✅ Add Model Product

---
+++ models/Product.js
@@ -0,0 +1,28 @@
+const Sequelize = require('sequelize');
+const connexion = require('../config/database');
+
+
+module.exports = connexion.define('products',{
+    id: {
+        type: Sequelize.INTEGER,
+        autoIncrement: true,
+        primaryKey: true,
+    },
+    name: {
+        type: Sequelize.STRING,
+        allowNull: false,
+    },
+    decsription: {
+        type: Sequelize.STRING,
+        allowNull: false,
+    },
+    price: {
+        type: Sequelize.STRING,
+        allowNull: false,
+    },
+    image: {
+        type: Sequelize.STRING,
+        allowNull: false,
+    }
+})
+
