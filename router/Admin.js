d2d960576368da7bc1b893b1b71eb51d95e3f48c
khadija <khadijalahmar8@gmail.com>
2022-03-31 15:38:26 +0000

    👷 Add Route Admin

---
+++ router/Admin.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const router = express.Router();
+const {AdminController } =require('../controllers');
+
+
+// Création Route Admin*
+
+// Route Product 
+router.route("/Admin/create")
+.post(authorizeWithRole("admin"),Admins.createProduct);
+
+router.route("/Admin/Products")
+.get(AdminController.getProduct)
+.put(authorizeWithRole("admin"),AdminController.updateProduct)
+.delete(authorizeWithRole("admin"),AdminController.deleteProduct)
+
+// Route User
+router.route("Admin/user")
+.post(AdminController.createUser)
+.get(AdminController.getUsers)
+.get(AdminController.getUserById)
+.put(AdminController.updateUser)
+
+
+
+
+module.export = router ;
