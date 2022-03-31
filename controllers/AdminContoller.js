c280de15823e99fe8622f51aa58b936901369c27
khadija <khadijalahmar8@gmail.com>
2022-03-31 16:49:04 +0000

    📌 Create Route Product and Methode

--- controllers/AdminContoller.js
+++ controllers/AdminContoller.js
@@ -1,1 +1,30 @@
 const express = require("express");
+const {User} =require('../models');
+
+
+
+const createProduct = async((req,res)=>{
+
+})
+
+const getProduct = async((req,res)=>{
+
+})
+const updateProduct = async((req,res)=>{
+
+})
+const deleteProduct = async((req,res)=>{
+
+})
+
+module.exports ={
+    createProduct,
+    getProduct,
+    updateProduct,
+    deleteProduct
+
+}
+
+
+
+
--- router/Admin.js
+++ router/Admin.js
@@ -4,22 +4,31 @@ const {AdminController } =require('../controllers');
 
 
 // Création Route Admin*
-
 // Route Product 
 router.route("/Admin/create")
-.post(authorizeWithRole("admin"),Admins.createProduct);
+.post(
+    // authorizeWithRole("admin"),
+    Admins.createProduct);
 
 router.route("/Admin/Products")
 .get(AdminController.getProduct)
-.put(authorizeWithRole("admin"),AdminController.updateProduct)
-.delete(authorizeWithRole("admin"),AdminController.deleteProduct)
+.put(
+    // authorizeWithRole("admin"),
+AdminController.updateProduct)
+.delete(
+    // authorizeWithRole("admin"),
+    AdminController.deleteProduct)
+
+// Route Cretion categorie 
+
+
 
 // Route User
-router.route("Admin/user")
-.post(AdminController.createUser)
-.get(AdminController.getUsers)
-.get(AdminController.getUserById)
-.put(AdminController.updateUser)
+// router.route("Admin/user")
+// .post(AdminController.createUser)
+// .get(AdminController.getUsers)
+// .get(AdminController.getUserById)
+// .put(AdminController.updateUser)
 
 
 
